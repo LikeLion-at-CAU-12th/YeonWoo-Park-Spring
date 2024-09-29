a35c92a2a3a203948fe6c3a76e4c1738c6a1c206
only4wxx <orchidbyw1@gmail.com>
2024-09-29 22:13:31 +0900

    [Feat] 스탠다드 과제 조건 만족하는 메소드 생성 및 테스트

--- src/main/java/com/example/spring_session/repository/MemberJpaRepository.java
+++ src/main/java/com/example/spring_session/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package com.example.spring_session.repository;
 
 import com.example.spring_session.domain.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsername(String username);
+
+    Page<Member> findByAgeGreaterThanEqual(int age, Pageable pageable); // 나이가 입력값보다 크거나 같은 요소
+    Page<Member> findByUsernameStartingWith(String word, Pageable pageable); // 이름이 주어진 값으로 시작하는 경우
 }
