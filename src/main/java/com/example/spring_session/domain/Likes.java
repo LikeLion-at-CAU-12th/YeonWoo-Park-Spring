2c86ab52420724e0d49e1379812b75088641aa4b
only4wxx <orchidbyw1@gmail.com>
2024-10-13 19:50:15 +0900

    [Feat] class 이름 수정

--- src/main/java/com/example/spring_session/domain/Likes.java
+++ src/main/java/com/example/spring_session/domain/Likes.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Like extends BaseTimeEntity {
+public class Likes extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "like_id")
     private Long id;
